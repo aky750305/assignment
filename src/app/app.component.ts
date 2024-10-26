c1e4875cd68335490b3d8835f4819677e2ecd7a6
aky750305 <aky7503051815@gmail.com>
2024-10-27 00:41:28 +0530

    audio setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,99 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { ChangeDetectorRef, Component, ViewChild } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
+import { AudioRecordingService } from './audio-recording.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [NgIf],
+  providers: [RouterOutlet, AudioRecordingService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'assignment';
+  @ViewChild('videoElement') videoElement: any;
+  isPlaying = false;
+  displayControls = true;
+  isAudioRecording = false;
+  audioRecordedTime: any;
+  audioBlobUrl: any;
+  audioBlob: any;
+  audioName: any;
+  audioStream: any;
+  audioConf = { audio: true };
+
+  constructor(
+    private ref: ChangeDetectorRef,
+    private audioRecordingService: AudioRecordingService,
+    private sanitizer: DomSanitizer
+  ) {
+
+
+    this.audioRecordingService.recordingFailed().subscribe(() => {
+      this.isAudioRecording = false;
+      this.ref.detectChanges();
+    });
+
+    this.audioRecordingService.getRecordedTime().subscribe((time) => {
+      this.audioRecordedTime = time;
+      this.ref.detectChanges();
+    });
+
+    this.audioRecordingService.getRecordedBlob().subscribe((data) => {
+      this.audioBlob = data.blob;
+      this.audioName = data.title;
+      this.audioBlobUrl = this.sanitizer.bypassSecurityTrustUrl(URL.createObjectURL(data.blob));
+      this.ref.detectChanges();
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  startAudioRecording() {
+    if (!this.isAudioRecording) {
+      this.isAudioRecording = true;
+      this.audioRecordingService.startRecording();
+    }
+  }
+
+  abortAudioRecording() {
+    if (this.isAudioRecording) {
+      this.isAudioRecording = false;
+      this.audioRecordingService.abortRecording();
+    }
+  }
+
+  stopAudioRecording() {
+    if (this.isAudioRecording) {
+      this.audioRecordingService.stopRecording();
+      this.isAudioRecording = false;
+    }
+  }
+
+  clearAudioRecordedData() {
+    this.audioBlobUrl = null;
+  }
+
+  downloadAudioRecordedData() {
+    this._downloadFile(this.audioBlob, 'audio/mp3', this.audioName);
+  }
+
+  ngOnDestroy(): void {
+    this.abortAudioRecording();
+  }
+
+  _downloadFile(data: any, type: string, filename: string): any {
+    const blob = new Blob([data], { type: type });
+    const url = window.URL.createObjectURL(blob);
+    const anchor = document.createElement('a');
+    anchor.download = filename;
+    anchor.href = url;
+    document.body.appendChild(anchor);
+    anchor.click();
+    document.body.removeChild(anchor);
+  }
 }
