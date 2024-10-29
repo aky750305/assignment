667a9f8b1502f5b3b061e9560150d4cd6af14478
aky750305 <aky7503051815@gmail.com>
2024-10-29 08:37:59 +0530

    changs

--- src/app/components/headers/headers.component.ts
+++ src/app/components/headers/headers.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+// import {MatDividerModule} from '@angular/material/divider';
 
 @Component({
   selector: 'app-headers',
@@ -8,5 +9,9 @@ import { Component } from '@angular/core';
   styleUrl: './headers.component.scss'
 })
 export class HeadersComponent {
-
+  dataSource = [];
+  
+  logout() {
+    console.log('logout');
+  }
 }
