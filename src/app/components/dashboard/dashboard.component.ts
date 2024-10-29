4352d01661406d3fa0ec35fcafc8cc445f9a947d
aky750305 <aky7503051815@gmail.com>
2024-10-29 08:41:51 +0530

    changs

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
-
+import { MatTableModule } from '@angular/material/table';
+import { DetailsComponent } from '../details/details.component';
+import {
+  MatDialogRef,
+  MatDialogModule,
+  MatDialog
+} from "@angular/material/dialog";
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule, MatDialogModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+  dataSource = [];
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  matDialogRef: any;
+  constructor(
+    // private matDialogRef: MatDialogRef,
+    private matDialog: MatDialog
+  ) {}
+
+  addData() {
+    // console.log('addData');
+    this.OpenModal();
+  }
+
+  OpenModal() {
+    this.matDialogRef = this.matDialog.open(DetailsComponent, {
+      disableClose: true
+    });
+
+    this.matDialogRef.afterClosed().subscribe((res:any) => {
+      if ((res == true)) {
+        // this.name = "";
+      }
+    });
+  }
 
 }
