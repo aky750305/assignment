cd2e2e61e36f95ba95119b98d92c9fe1c9284a2f
aky750305 <aky7503051815@gmail.com>
2024-10-30 11:39:50 +0530

    clean up

--- src/app/app.config.ts
+++
@@ -1,9 +0,0 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';
-import { provideToastr } from 'ngx-toastr';
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideAnimations(), provideToastr(), provideRouter(routes)]
-};
